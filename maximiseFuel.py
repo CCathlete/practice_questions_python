537738fa41ad1a5b1bd94cb2614cfae5a41d9f84
ccat <ccathlete01@gmail.com>
2024-03-08 15:33:59 +0200

    fuelMax solution ready for testing.

--- maximiseFuel.py
+++ maximiseFuel.py
@@ -1,18 +1,21 @@
 
-"""Consider a car with a max fuel capacity of n litres. Two infinite-capacity containers are 
-available, one containing petrol and the other containing diesel. Each move allows you to
-transfer a (variable) litres of petrol from the petrol container or b litres of diesel from the 
+"""Consider a car with a max fuel capacity of n litres. Two infinite-
+capacity containers are available, one containing petrol and the other 
+containing diesel. Each move allows you to transfer a (variable) litres 
+of petrol from the petrol container or b litres of diesel from the 
 diesel container into the car.
 
-The task is to maximise the total fuel in the car, subject to the following conditions:
+The task is to maximise the total fuel in the car, subject to the 
+following conditions:
 
 1. The total fuel in the car cannot exceed n litres.
-2. Either a litres of petrol or b litres of diesel can be added in one move.
-3. The current fuel level of the car can be decreased by half (round down in the case of float 
-values) at most once.
+2. Either a litres of petrol or b litres of diesel can be added in one 
+move.
+3. The current fuel level of the car can be decreased by half (round 
+down in the case of float values) at most once.
 
-The goal  is to formulate an approach that determines the maximum achievable fuel amount in the 
-car.
+The goal is to formulate an approach that determines the maximum 
+achievable fuel amount in the car.
 -- We want to reach as close to n by steps of adding a or b.
 """
 class tank:
@@ -40,7 +43,8 @@ class tank:
             cap-petrol_amount < cap-diesel_amount
         take_diesel = cap-diesel_amount >= 0 and\
             cap-petrol_amount >= cap-diesel_amount
-        
+        # If we can't fill with petrol nor with diesel
+        # the func returns 'neither'.
         if not take_petrol and not take_diesel:
             return 'neither'
         if take_petrol:
@@ -49,12 +53,31 @@ class tank:
             return 'diesel'
 
 
-
-
-
-
-
 class Solution:
     def maximizeTheFuel(self, n:int , A:int , B:int ) -> int :
-        amount_left = n
-        currently_in = 0
+        tanky = tank(n)
+        previous_quantity = None
+        current_quantity = tanky.currently_in
+        while current_quantity != previous_quantity:
+            previous_quantity = tanky.currently_in
+            which_fuel = tanky.decide_which_fuel(petrol_amount=A, 
+                                                 diesel_amount=B) 
+            # During the following condition the quantity will definitely 
+            # change unless we've reached the max amount possible.
+            if which_fuel == 'neither':
+               tanky.half_quantity()
+            elif(which_fuel == 'petrol'):
+                tanky.put_fuel(A)
+            elif(which_fuel == 'diesel'):
+                tanky.put_fuel(B)
+            current_quantity = tanky.currently_in
+
+        
+        
+        
+        
+        
+        
+        
+        
+
